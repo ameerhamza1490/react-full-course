55fbfb14207586984bcf071a3c405eae72457cce
ameerhamza1490 <ameerhamza1490@gmail.com>
2026-03-06 08:17:02 +0500

    Fixed

--- Simple Cards/src/Components/Cards.jsx
+++ Simple Cards/src/Components/Cards.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import React from "react";
+import { Bookmark } from "lucide-react";
 
-const Cards = () => {
+const Cards = (props) => {
   return (
-    <div>
-      Cards
-    </div>
-  )
-}
+    <div className="container">
+      <div className="header">
+        <div className="logo">
+          <img src={props.logo} alt="" />
+        </div>
 
-export default Cards
+        <button className="save-btn">
+          Save <Bookmark size={15} strokeWidth={1} />
+        </button>
+      </div>
+      <div className="content">
+        <h4>
+          {props.company} <span className="span">{props.datePosted}</span>
+        </h4>
+        <h2>{props.post}</h2>
+        <span className="tags">{props.tag1}</span>
+        <span className="tags">{props.tag2}</span>
+      </div>
+      <div className="footer">
+        <div className="bottom">
+          <h3>{props.pay}</h3>
+          <span className="span">{props.location}</span>
+        </div>
+        <button className="btn">Apply Now</button>
+      </div>
+    </div>
+  );
+};
+
+export default Cards;
