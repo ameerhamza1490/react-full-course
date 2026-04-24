6d0e09175a9d43dc4af92477613fb6ba0108e71f
ameerhamza1490 <ameerhamza1490@gmail.com>
2026-04-25 00:35:44 +0500

    updated

--- Use State/src/App.jsx
+++ Use State/src/App.jsx
@@ -7,6 +7,8 @@ const App = () => {
     userName: "Ameer Hamza",
     age: 32,
   });
+
+  const [a, setA] = useState(10);
   const btnClick = () => {
     const newNum = [...num];
     newNum.push(50);
@@ -21,6 +23,14 @@ const App = () => {
     setUserDetails((prev) => ({ ...prev, userName: "Hamza Khan" }));
   };
 
+  //Batch update in react
+  const addA = () => {
+    setA((prev) => prev + 1);
+    setA((prev) => prev + 1);
+    setA((prev) => prev + 1);
+  };
+
+
   return (
     <div>
       <div>
@@ -32,6 +42,10 @@ const App = () => {
           {userDetails.userName}, {userDetails.age}
         </h1>
         <button onClick={changeName}>Click here to change User Name.</button>
+      </div>
+      <div>
+        <h1>{a}</h1>
+        <button onClick={addA}>Click here to add 3</button>
       </div>
     </div>
   );
